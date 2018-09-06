10d3e5088eabb54eb134c12d687fb68ef6345241
tactreconx <league@WTS-MM-11.local>
2018-09-05 18:27:16 -0700

    Hangman (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -4,30 +4,36 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class HangMan{
-	
+public class HangMan{ 
+	void Setup() {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
+	frame.add(panel);
+	panel.add(label1);
+	panel.add(label2);
+	panel.add(label3);
+	}
+	static Stack<String> s = new Stack<>();
 	public static void main(String[] args) {
 	String x =	JOptionPane.showInputDialog("how many words would you like to have");
 		int y = Integer.parseInt(x);
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("src/intro_to_file_io/test.txt"));
-			
-			String line = br.readLine();
-			for (int i = 0; i < y; i++) {
-				System.out.println(line);
-				line = br.readLine();
+		for (int i = 0; i < y; i++) {
+			String word = Utilities.readRandomLineFromFile("dictionary.txt");
+			if (!s.contains(word)) {
+				s.push(word);
 			}
-			
-			br.close();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 	
