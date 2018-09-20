334c94fa03cf15867a73b3fd3b3fff238f137cb2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-19 18:27:03 -0700

    HangMan (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -14,24 +14,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class HangMan{ 
+	String currentWord;
+	String hiddenWord;
+	int lives = 0;
 	void Setup() {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JLabel label1 = new JLabel();
-	JLabel label2 = new JLabel();
-	JLabel label3 = new JLabel();
-	frame.add(panel);
-	panel.add(label1);
-	panel.add(label2);
-	panel.add(label3);
-	frame.setVisible(true);
-	frame.pack();
-	}
-	static Stack<String> s = new Stack<>();
-	public static void main(String[] args) {
-		HangMan hang = new HangMan();
-		hang.Setup();
-	String x =	JOptionPane.showInputDialog("how many words would you like to have");
+		String x =	JOptionPane.showInputDialog("how many words would you like to have");
 		int y = Integer.parseInt(x);
 		for (int i = 0; i < y; i++) {
 			String word = Utilities.readRandomLineFromFile("src/_04_HangMan/dictionary.txt");
@@ -39,6 +26,30 @@ public class HangMan{
 				s.push(word);
 			}
 		}
+		currentWord = s.pop();
+		hiddenWord="";
+		for (int i = 0; i < currentWord.length(); i++) {
+			hiddenWord += " _ ";
+		}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label1 = new JLabel(hiddenWord);
+	JLabel label2 = new JLabel("Lives remaining:");
+	JLabel label3 = new JLabel(Integer.toString(lives));
+	frame.add(panel);
+	panel.add(label1);
+	panel.add(label2);
+	panel.add(label3);
+	frame.setVisible(true);
+
+	frame.pack();
+	
+	
+	}
+	static Stack<String> s = new Stack<>();
+	public static void main(String[] args) {
+		HangMan hang = new HangMan();
+		hang.Setup();	
 	}
 	
 }
