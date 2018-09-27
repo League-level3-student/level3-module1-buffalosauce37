384b8210af62e779a0fb8315b375916ea0195f5d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-26 18:24:44 -0700

    HangMan (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,10 +15,23 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class HangMan{ 
+public class HangMan implements KeyListener { 
 	String currentWord;
 	String hiddenWord;
-	int lives = 0;
+	int lives = 10;
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label1 = new JLabel(hiddenWord);
+	JLabel label2 = new JLabel("Lives remaining:");
+	JLabel label3 = new JLabel(Integer.toString(lives));
+	void startGame() {
+		currentWord = s.pop();
+		hiddenWord="";
+		for (int i = 0; i < currentWord.length(); i++) {
+			hiddenWord += "_";
+		}
+		lives = 10;
+	}
 	void Setup() {
 		String x =	JOptionPane.showInputDialog("how many words would you like to have");
 		int y = Integer.parseInt(x);
@@ -26,30 +41,58 @@ public class HangMan{
 				s.push(word);
 			}
 		}
-		currentWord = s.pop();
-		hiddenWord="";
-		for (int i = 0; i < currentWord.length(); i++) {
-			hiddenWord += " _ ";
-		}
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JLabel label1 = new JLabel(hiddenWord);
-	JLabel label2 = new JLabel("Lives remaining:");
-	JLabel label3 = new JLabel(Integer.toString(lives));
 	frame.add(panel);
 	panel.add(label1);
 	panel.add(label2);
 	panel.add(label3);
+	frame.addKeyListener(this);
+	label1.setText(hiddenWord);
 	frame.setVisible(true);
 
 	frame.pack();
 	
-	
+	startGame();
 	}
 	static Stack<String> s = new Stack<>();
 	public static void main(String[] args) {
 		HangMan hang = new HangMan();
 		hang.Setup();	
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(currentWord.contains(Character.toString(e.getKeyChar()))) {
+			String newHiddenWord = "";
+			for (int i = 0; i < currentWord.length(); i++) {
+				if(currentWord.charAt(i) == e.getKeyChar()) {
+					newHiddenWord += e.getKeyChar();
+				}
+				else {
+					newHiddenWord += hiddenWord.charAt(i);
+				}
+			}
+			hiddenWord = newHiddenWord;
+			label1.setText(newHiddenWord);
+			frame.pack();
+		}
+		else if(lives <= 0) {
+			JOptionPane.showMessageDialog(null, "game over the work was " + currentWord);
+		}
+		else{
+			lives -= 1;
+			label3.setText(Integer.toString(lives));
+		}
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
